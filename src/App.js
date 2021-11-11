036cef7d92aa8ea931221c16d35009b00d330fc6
Bharatchandran <bharatchandrankr@gmail.com>
2021-11-11 21:13:17 +0530

    widget done working on firebase

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import "./App.css";
 import Feed from "./Feed";
 import Header from "./Header";
+import Login from "./Login";
 import Sidebar from "./Sidebar";
+import Widgets from "./Widget";
 
 function App() {
+  const user = null;
   return (
     <div className="App">
-      <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
 
-      <div className="app__body">
-        <Sidebar />
-
-        <Feed />
-        {/* Widget */}
-      </div>
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />{" "}
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyBjpH8QgcVkE3Q960dsS3Eugv0kRzLQQps",
+  authDomain: "facebook-clone-6f5e0.firebaseapp.com",
+  projectId: "facebook-clone-6f5e0",
+  storageBucket: "facebook-clone-6f5e0.appspot.com",
+  messagingSenderId: "701190955953",
+  appId: "1:701190955953:web:d4f7ad16f0464a9133d390",
+  measurementId: "G-NZ6Z78BQR7",
+};
+const firebaseApp = initializeApp(firebaseConfig);
